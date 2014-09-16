5a3ea4dfd6ec0fc90884423a5cda9bd37a82bcae
VovaPryst <vpryst@gmail.com>
2014-09-16 19:11:23 -0400

    g

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -3,6 +3,7 @@
   Game of Life program using allegro */
 #include <iostream>
 #include <iostream>
+#include <apmatrix>
 #include <allegro.h>
 
 using namespace std;
@@ -10,8 +11,7 @@ using namespace std;
 #define Rows 48
 #define Columns 64
 
-apmatrix<int> grid(Rows,Columns);
-
+void populator(apmatrix<int> &a); //temporary function that populates the matrix randomly
 void read_file();
 void grid_create();
 void neighbor_check();
@@ -19,23 +19,39 @@ bool alive_check();
 void birth();
 
 int main(){
-  BITMAP *buffer=NULL;
+	apmatrix<int> grid(Rows,Columns); //create the world
+	
+	populator(apmatrix<int> grid); //populate the world
+	
+	/*BITMAP *buffer=NULL;
   
   //allegro initialisation
   allegro_init();
   
-  read_file();
+  //read_file();
   grid_create();
   neighbor_check();
   alive_check();
-  birth();
+  birth();*/
   return 0;
 }
 
-void read_file(){
+void populator(apmatrix<int> &grid){
+	for(int x = 0; x < grid.numrows(); x++)
+		{
+			for(int y = 0; y < grid.numcols(); y++)
+			{
+				grid[x, y] = rand() % 1; //seed all parts of the matrix with pseudo-random boolean 'alive's or 'dead's
+				cout << grid[x, y] << " ";
+			}
+			cout << endl;
+		}
+}
+
+/*void read_file(){
   //read in the file here
   //use a structure i.e. Ap matrix
-}
+}*/
 
 void grid_create(){
   //create the grid
